cfdbd89f153883eee0c52bdd64048e85b01bdb64
Hà Hoàng Hiệp <114068499+hhiep1504@users.noreply.github.com>
2024-08-22 12:11:53 +0700

    Update __init__.py

--- data_v2/datasets/image/__init__.py
+++ data_v2/datasets/image/__init__.py
@@ -18,5 +18,4 @@ from .cuhk03_labeled import CUHK03_Labeled
 from .cuhk03_splited import CUHK03_splited
 from .msmt17 import MSMT17
 from .mot17 import MOT17
-from .veri import VeRi
 from .aicity24 import AICity24Balanced
